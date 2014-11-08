7c8e76d5f97140e8112220a8b06bea3acc260d99
jmhwang7 <jmhwang7@gmail.com>
2014-11-07 22:46:06 -0600

    Try heroku again.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express'),
 	app = express();
 
+app.set('port', (process.env.PORT || 3000));
 app.get('/', function(req, res) {
 	res.send('Hello World!');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(app.get('port'), function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
