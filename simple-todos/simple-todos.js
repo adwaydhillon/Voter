af80f5f8775fc2485070cc338596d4d9e5ba6c4e
srijansood <Srijan@Srijans-MacBook-Pro.local>
2014-11-07 23:44:32 -0600

    Meteor test

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -28,4 +28,14 @@ if (Meteor.isClient) {
   }
 });
 
+Template.task.events({
+  "click .toggle-checked": function () {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {$set: {checked: ! this.checked}});
+  },
+  "click .delete": function () {
+    Tasks.remove(this._id);
+  }
+});
+
 }
