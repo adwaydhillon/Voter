a31e06c00a6ea764394f5270eee1d416411275d9
srijansood <Srijan@Srijans-MacBook-Pro.local>
2014-11-07 23:58:22 -0600

    Session and incomplete count

--- voter-app/voter-app.js
+++ voter-app/voter-app.js
@@ -3,11 +3,22 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: function () {
-      // Show newest tasks first
+  tasks: function () {
+    if (Session.get("hideCompleted")) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+    } else {
+      // Otherwise, return all of the tasks
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
-  });
+  },
+  hideCompleted: function () {
+    return Session.get("hideCompleted");
+    },
+  incompleteCount: function () {
+  return Tasks.find({checked: {$ne: true}}).count();
+    }
+});
 
   Template.body.events({
   "submit .new-task": function (event) {
@@ -25,7 +36,10 @@ if (Meteor.isClient) {
 
     // Prevent default form submit
     return false;
-  }
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
 });
 
 Template.task.events({
