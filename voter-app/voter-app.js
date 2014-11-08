a3af03702ba0a56c9a8e0d6b2f30fb482bfe5af9
jmhwang7 <jmhwang7@gmail.com>
2014-11-08 01:26:14 -0600

    twilio-meteor

--- voter-app/voter-app.js
+++ voter-app/voter-app.js
@@ -38,4 +38,29 @@ Template.task.events({
   }
 });
 
+Template.twilio_test.events({
+  "click button": function() {
+    //
+    console.log("hey");
+    Meteor.call("sendsms");
+  }
+});
 }
+
+Meteor.methods({
+  sendsms: function() {
+    var accountSid = 'AC05a65b25f93c0661020d39873a925618';
+    var authToken = "7f9b1c480cb2e82a540eec3ccea2a502";
+    var client = Twilio(accountSid, authToken);
+    client.sendSms({
+        body: "Adway is a chut",
+        to: "+16787561965",
+        from: "+16787854359"
+    }, function(err, message) {
+        if (err) {
+            console.log(err.message);
+        }
+        process.stdout.write(message.sid);
+    });
+  }
+})
