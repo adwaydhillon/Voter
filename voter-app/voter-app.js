9c04b3ae14541390e641c11aa31a63347bcaf9ae
srijansood <srijansood@gmail.com>
2014-11-08 09:40:25 -0600

    Voter collection

--- voter-app/voter-app.js
+++ voter-app/voter-app.js
@@ -1,4 +1,4 @@
-Tasks = new Mongo.Collection("tasks");
+Votes = new Mongo.Collection("tasks");
 //twilio = Twilio("AC21f850538ec9bb250cd0de8b5c0badb3", "eaa95b3091b8866e36c1ec9aa82588e5");
 
 if (Meteor.isClient) {
@@ -7,17 +7,17 @@ if (Meteor.isClient) {
   tasks: function () {
     if (Session.get("hideCompleted")) {
       // If hide completed is checked, filter tasks
-      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      return Votes.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
     } else {
       // Otherwise, return all of the tasks
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      return Votes.find({}, {sort: {createdAt: -1}});
     }
   },
   hideCompleted: function () {
     return Session.get("hideCompleted");
     },
   incompleteCount: function () {
-  return Tasks.find({checked: {$ne: true}}).count();
+  return Votes.find({checked: {$ne: true}}).count();
     }
 });
 
@@ -27,7 +27,7 @@ if (Meteor.isClient) {
 
     var text = event.target.text.value;
 
-    Tasks.insert({
+    Votes.insert({
     text: text,
     createdAt: new Date(),            // current time
     owner: Meteor.userId(),           // _id of logged in user
@@ -51,10 +51,10 @@ if (Meteor.isClient) {
 Template.task.events({
   "click .toggle-checked": function () {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    Votes.update(this._id, {$set: {checked: ! this.checked}});
   },
   "click .delete": function () {
-    Tasks.remove(this._id);
+    Votes.remove(this._id);
   }
 });
 
