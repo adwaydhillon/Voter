6a4082402b411643324aeaaea176d1a8d9038e76
srijansood <srijansood@gmail.com>
2014-11-09 15:13:24 -0600

    Continue

---
+++ voter-app/lib/routes.js
@@ -0,0 +1,14 @@
+// /**
+// * Setup
+// */
+// Router.onBeforeAction(Iron.Router.bodyParser.urlencoded({
+//     extended: false
+// }));
+//
+// /**
+// * Routes
+// */
+// Router.route('/vote', {where: 'client'})
+// .post(function(req, res) {
+//     console.log(req.body);
+// });
