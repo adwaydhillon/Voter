ee054c6ed11866e74f5a58d1e981471203ce0d33
srijansood <srijansood@gmail.com>
2014-11-08 21:34:24 -0600

    Converting tasks to vote items

--- voter-app/client/voter-app.js
+++ voter-app/client/voter-app.js
@@ -2,17 +2,20 @@
 * Collections
 */
 Votes = new Mongo.Collection("votes");
+Tasks = new Mongo.Collection("tasks");
 
 // This code only runs on the client
 Template.body.helpers({
     tasks: function () {
-        if (Session.get("hideCompleted")) {
+        // if (Session.get("hideCompleted")) {
             // If hide completed is checked, filter tasks
-            return Votes.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-        } else {
-            // Otherwise, return all of the tasks
-            return Votes.find({}, {sort: {createdAt: -1}});
-        }
+        //     return Votes.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+        // } else {
+        //     // Otherwise, return all of the tasks
+        //     return Votes.find({}, {sort: {createdAt: -1}});
+        // }
+        // return Votes.find({}, {sort: {createdAt: -1}});
+        return Tasks.find({}, {sort: {createdAt: -1}});
     },
     hideCompleted: function () {
         return Session.get("hideCompleted");
@@ -23,17 +26,23 @@ Template.body.helpers({
 });
 
 Template.body.events({
-    "submit .new-task": function (event) {
+    "submit .new-voteitem": function (event) {
         // This function is called when the new task form is submitted
 
         var text = event.target.text.value;
 
-        Votes.insert({
-            text: text,
-            createdAt: new Date(),            // current time
-            owner: Meteor.userId(),           // _id of logged in user
-            username: Meteor.user().username  // username of logged in user
-        });
+        // Votes.insert({
+        //     text: text,
+        //     createdAt: new Date(),            // current time
+        //     owner: Meteor.userId(),           // _id of logged in user
+        //     // username: Meteor.user().username  // username of logged in user
+        // });
+
+        Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
 
         // Clear form
         event.target.text.value = "";
@@ -43,9 +52,25 @@ Template.body.events({
     },
     "change .hide-completed input": function (event) {
         Session.set("hideCompleted", event.target.checked);
-    }
+    },
+        "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+      var text = event.target.text.value;
 
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
 });
+
+
 
 Template.task.events({
     "click .toggle-checked": function () {
@@ -57,12 +82,7 @@ Template.task.events({
     }
 });
 
-Template.twilio_test.events({
-    "click button": function() {
-        console.log("hey");
-        Meteor.call("sendSMS", "Testing", "+16787561965");
-    }
-});
+
 // Login Functionality
 Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
@@ -76,6 +96,16 @@ Template.task.events({
     "click .delete": function () {
         Votes.remove(this._id);
     }
+});
+
+Template.dyform2.events({
+"addInput": function(divName){
+          var counter = 1;
+          var newdiv = document.createElement('div');
+          newdiv.innerHTML = "Entry " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
+          document.getElementById(divName).appendChild(newdiv);
+          counter++;
+     }
 });
 
 Template.twil.events({
@@ -118,7 +148,8 @@ Template.dyform.events({
             });
         });
     }
-})
+});
+
 
 /**
 * Server methods that Client can call.
