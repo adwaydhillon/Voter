5075526c88e819b659f1665fa86bf795831f273e
srijansood <srijansood@gmail.com>
2014-11-08 23:03:16 -0600

    checkboxes completely removed

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -17,28 +17,19 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
 
       Meteor.call("addTask", text);
+      Meteor.call("setPrivate", this._id, ! this.private);
 
       // Clear form
       event.target.text.value = "";
 
       // Prevent default form submit
       return false;
-    },
-    "change .hide-completed input": function (event) {
-      Session.set("hideCompleted", event.target.checked);
     }
   });
 
   Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Meteor.call("setChecked", this._id, ! this.checked);
-    },
     "click .delete": function () {
       Meteor.call("deleteTask", this._id);
-    },
-    "click .toggle-private": function () {
-      Meteor.call("setPrivate", this._id, ! this.private);
     }
   });
 
@@ -75,15 +66,6 @@ Meteor.methods({
     }
 
     Tasks.remove(taskId);
-  },
-  setChecked: function (taskId, setChecked) {
-    var task = Tasks.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      // If the task is private, make sure only the owner can check it off
-      throw new Meteor.Error("not-authorized");
-    }
-
-    Tasks.update(taskId, { $set: { checked: setChecked} });
   },
   setPrivate: function (taskId, setToPrivate) {
     var task = Tasks.findOne(taskId);
