b3caf025b56029266c52db4ef09907a2fe32a265
srijansood <srijansood@gmail.com>
2014-11-08 22:34:01 -0600

    Checkboxes

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -6,19 +6,8 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     tasks: function () {
-      if (Session.get("hideCompleted")) {
-        // If hide completed is checked, filter tasks
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      } else {
-        // Otherwise, return all of the tasks
+      //if (Session.get("hideCompleted")) {
         return Tasks.find({}, {sort: {createdAt: -1}});
-      }
-    },
-    hideCompleted: function () {
-      return Session.get("hideCompleted");
-    },
-    incompleteCount: function () {
-      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
