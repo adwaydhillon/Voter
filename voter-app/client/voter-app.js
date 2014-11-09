98888385a31a7639329318def8fcc25fbcd2661e
jmhwang7 <jmhwang7@gmail.com>
2014-11-09 02:43:10 -0600

    Modified front-end working.

--- voter-app/client/voter-app.js
+++ voter-app/client/voter-app.js
@@ -1,79 +1,40 @@
-/**
-* Collections
-*/
-Tasks = new Mongo.Collection("tasks");
-
 // This code only runs on the client
-if (Meteor.isClient) {
- Template.body.helpers({
-    tasks: function () {
-      // Show newest tasks first
+Meteor.subscribe("tasks");
+
+Template.body.helpers({
+  tasks: function () {
+    //if (Session.get("hideCompleted")) {
       return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
+  }
+});
 
 Template.body.events({
-    "submit .new-task": function (event) {
-      // This function is called when the new task form is submitted
-      var text = event.target.text.value;
+  "submit .new-task": function (event) {
+    // This function is called when the new task form is submitted
+    var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
+    Meteor.call("addTask", text);
 
-      // Clear form
-      event.target.text.value = "";
+    // Clear form
+    event.target.text.value = "";
 
-      // Prevent default form submit
-      return false;
-    }
-  });
-
-
+    // Prevent default form submit
+    return false;
+  }
+});
 
 Template.task.events({
-    "click .toggle-checked": function () {
-        // Set the checked property to the opposite of its current value
-        Tasks.update(this._id, {$set: {checked: ! this.checked}});
-    },
-    "click .delete": function () {
-        Tasks.remove(this._id);
-    }
-});
-}
-// Login Functionality
-Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
-});
-
-
-Template.twil.events({
-    "click #send": function() {
-        console.log("Send Attempt");
-        Meteor.call("sendSMS", "Testing", "+16787561965");
-    },
-    "click #rec": function() {
-        console.log("Rec attempt")
-        Meteor.call("recsms")
-    }
-});
-
-
-
-/**
-* Server methods that Client can call.
-*/
-Meteor.methods({
-  recsms: function() {
-    var twil_client = Twilio(accountSid, authToken);
-    twil_client.listSms({
-      from:'+14049407775'
-    }, function (err, responseData) {
-      responseData.smsMessages.forEach(function(message) {
-        console.log('Message sent on: '+message.dateCreated.toLocaleDateString());
-        console.log(message.body);
-      });
-    });
+  "click .delete": function () {
+    Meteor.call("deleteTask", this._id);
   }
+});
+
+Template.task.helpers({
+  isOwner: function () {
+    return this.owner === Meteor.userId();
+  }
+});
+
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
 });
--- voter-app/server/voter-app.js
+++ voter-app/server/voter-app.js
@@ -94,5 +94,37 @@ Meteor.methods({
 
 		summary["total_votes"] = sum_votes;
 		return summary;
+	},
+	addTask: function (text) {
+	  // Make sure the user is logged in before inserting a task
+	  if (! Meteor.userId()) {
+	    throw new Meteor.Error("not-authorized");
+	  }
+
+	  Tasks.insert({
+	    text: text,
+	    createdAt: new Date(),
+	    owner: Meteor.userId(),
+	    username: Meteor.user().username,
+	    private: true //Every task is private by default
+	  });
+	},
+	deleteTask: function (taskId) {
+	  var task = Tasks.findOne(taskId);
+	  if (task.owner !== Meteor.userId()) {
+	    //Make sure only the owner can delete it
+	    throw new Meteor.Error("not-authorized");
+	  }
+
+	  Tasks.remove(taskId);
 	}
+});
+
+Meteor.publish("tasks", function () {
+  return Tasks.find({
+    $or: [
+      { private: {$ne: true} },
+      { owner: this.userId }
+    ]
+  });
 });
